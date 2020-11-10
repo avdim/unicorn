94bce53aa55a10437a5ff4f22a04c8a4b8b91102
avdim <avdim@mail.ru>
2020-11-10 13:13:08 +0300

    todos

--- lib-github/src/main/java/com/sample/todo.kt
+++ lib-github/src/main/java/com/sample/todo.kt
@@ -5,4 +5,5 @@ package com.sample
 
 //todo generate GraphQL
 // https://github.com/ExpediaGroup/graphql-kotlin/tree/5bc6b4a72211ab7b8040116f14061a12ec61ecf9/examples/client/gradle-client/src/main/resources
+// https://expediagroup.github.io/graphql-kotlin/docs/client/client-overview
 
