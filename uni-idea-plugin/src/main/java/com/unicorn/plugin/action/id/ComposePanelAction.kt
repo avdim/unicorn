ca24aaa6041e509290d6fa845a481c87d005fdc4
avdim <avdim@mail.ru>
2020-12-05 08:10:07 +0300

    ComposePanelAction

--- uni-idea-plugin/src/main/java/com/unicorn/plugin/action/id/ComposePanelAction.kt
+++ uni-idea-plugin/src/main/java/com/unicorn/plugin/action/id/ComposePanelAction.kt
@@ -6,17 +6,17 @@ import androidx.compose.material.Text
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 import com.intellij.ui.layout.LayoutBuilder
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
 import com.unicorn.plugin.ui.showDialog
+import com.unicorn.plugin.ui.showPanelDialog
+import javax.swing.JPanel
 
 @Suppress("ComponentNotRegistered")
 class ComposePanelAction : UniAction(), DumbAware {
 
   override fun actionPerformed(event: AnActionEvent) {
     val composePanel = ComposePanel()
-    showDialog(composePanel)
-    composePanel.setSize(400, 200)
+    val dialog = showDialog(composePanel)
+    dialog.setSize(400, 700)
     composePanel.setContent {
       Column {
         Text("Hello Compose")
