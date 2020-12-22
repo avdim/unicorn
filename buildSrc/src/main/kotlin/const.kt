e2c317506fb68ee2c309a87ea2de286199735d6b
avdim <avdim@mail.ru>
2020-12-22 13:25:52 +0300

    ktor 1.5.0

--- buildSrc/src/main/kotlin/const.kt
+++ buildSrc/src/main/kotlin/const.kt
@@ -25,7 +25,7 @@ val USE_KOTLIN_DEV_REPOSITORY = true
 val KOTLIN_VERSION = "1.4.21"
 val SERIALIZATION_VERSION = "1.0.1"
 val COROUTINE_VERSION = "1.4.2"
-val KTOR_VERSION = "1.4.3"
+val KTOR_VERSION = "1.5.0"
 val LOG_MAVEN_ARTIFACT = if (DEBUG_JVM) "ch.qos.logback:logback-classic:1.2.3" else "org.slf4j:slf4j-simple:1.7.28"
 
 //https://github.com/Kotlin/kotlinx.coroutines/commit/e37aec4edd09bfb7f622e113553aa88a0a5bd27c
