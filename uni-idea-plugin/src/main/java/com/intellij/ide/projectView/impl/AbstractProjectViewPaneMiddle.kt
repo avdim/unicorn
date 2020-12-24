8d90fad246fe759deeba1c0ee78f9dbcfd7c5891
avdim <avdim@mail.ru>
2020-12-24 18:13:33 +0300

    remove redundant code

--- uni-idea-plugin/src/main/java/com/intellij/ide/projectView/impl/AbstractProjectViewPaneMiddle.kt
+++ uni-idea-plugin/src/main/java/com/intellij/ide/projectView/impl/AbstractProjectViewPaneMiddle.kt
@@ -2,14 +2,8 @@
 
 package com.intellij.ide.projectView.impl
 
-import com.intellij.ide.util.treeView.AbstractTreeBuilder
-import com.intellij.ide.util.treeView.NodeDescriptor
 import com.intellij.openapi.project.Project
 
 abstract class AbstractProjectViewPaneMiddle(project: Project) : AbstractProjectViewPane2(project) {
-
-  override fun installComparator(builder: AbstractTreeBuilder, comparator: Comparator<in NodeDescriptor<*>>) {
-    super.installComparator(builder, comparator)
-  }
 
 }
