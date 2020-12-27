22a75d5cb6d8b1653d299c2cd44165a02dcd222b
avdim <avdim@mail.ru>
2020-12-27 18:25:08 +0300

    getFileColorFor

--- uni-idea-plugin/src/main/java/ru/tutu/idea/file/ProjectViewTree2.kt
+++ uni-idea-plugin/src/main/java/ru/tutu/idea/file/ProjectViewTree2.kt
@@ -2,78 +2,70 @@
 package ru.tutu.idea.file
 
 import com.intellij.ide.dnd.aware.DnDAwareTree
-import javax.swing.tree.TreeCellRenderer
 import com.intellij.ide.projectView.impl.ProjectViewRenderer
-import javax.swing.tree.DefaultMutableTreeNode
-import ru.tutu.idea.file.ProjectViewTree2
-import java.lang.IllegalStateException
-import ru.tutu.idea.file.ProjectViewTreeHelpers
-import com.intellij.ui.DirtyUI
-import com.intellij.psi.PsiElement
-import com.intellij.ide.projectView.ProjectViewNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.newvfs.VfsPresentationUtil
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiUtilCore
+import com.intellij.ui.DirtyUI
+import com.intellij.ui.JBColor
 import com.intellij.ui.popup.HintUpdateSupply
-import com.intellij.util.ObjectUtils
-import com.intellij.util.ui.tree.TreeUtil
+import com.unicorn.Uni
 import java.awt.Color
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.TreeCellRenderer
 import javax.swing.tree.TreeModel
 
 open class ProjectViewTree2(model: TreeModel?) : DnDAwareTree(null as TreeModel?) {
-    /**
-     * @return custom renderer for tree nodes
-     */
-    private fun createCellRenderer(): TreeCellRenderer {
-        return ProjectViewRenderer()
-    }
+  /**
+   * @return custom renderer for tree nodes
+   */
+  private fun createCellRenderer(): TreeCellRenderer {
+    return ProjectViewRenderer()
+  }
 
-    override fun setToggleClickCount(count: Int) {
-        if (count != 2) LOG.info(IllegalStateException("setToggleClickCount: unexpected count = $count"))
-        super.setToggleClickCount(count)
+  override fun setToggleClickCount(count: Int) {
+    if (count != 2) {
+      Uni.log.error {
+        "setToggleClickCount: unexpected count = $count"
+      }
     }
+    super.setToggleClickCount(count)
+  }
 
-    override fun isFileColorsEnabled(): Boolean {
-        return ProjectViewTreeHelpers.isFileColorsEnabledFor(this)
+  override fun isFileColorsEnabled(): Boolean {
+    return true
+  }
+
+  @DirtyUI
+  override fun getFileColorFor(obj: Any): Color? {
+    var obj: Any? = obj
+    if (obj is DefaultMutableTreeNode) {
+      obj = obj.userObject
     }
-
-    @DirtyUI
-    override fun getFileColorFor(obj: Any): Color? {
-        var obj: Any? = obj
-        if (obj is DefaultMutableTreeNode) {
-            obj = obj.userObject
+    if (obj is AbstractTreeNode<*>) {
+      val value = obj.value
+      if (value is PsiElement) {
+        val psi = value
+        if (!psi.isValid) {
+          return JBColor.RED
         }
-        if (obj is AbstractTreeNode<*>) {
-            val value = obj.value
-            if (value is PsiElement) {
-                return ProjectViewTreeHelpers.getColorForElement(value)
-            }
+        val file = PsiUtilCore.getVirtualFile(psi)
+        if (file != null) {
+          if (file.isDirectory) {
+            return JBColor.WHITE
+          } else {
+            return JBColor.WHITE
+          }
         }
-        if (obj is ProjectViewNode<*>) {
-            val node = obj
-            val file = node.virtualFile
-            if (file != null) {
-                val project = node.project
-                if (project != null && !project.isDisposed) {
-                    val color = VfsPresentationUtil.getFileBackgroundColor(project, file)
-                    if (color != null) return color
-                }
-            }
-        }
-        return null
+      }
     }
+    return JBColor.BLACK
+  }
 
-    companion object {
-        private val LOG = Logger.getInstance(
-            ProjectViewTree2::class.java
-        )
-    }
-
-    init {
-        isLargeModel = true
-        setModel(model)
-        setCellRenderer(createCellRenderer())
-        HintUpdateSupply.installDataContextHintUpdateSupply(this)
-    }
+  init {
+    isLargeModel = true
+    setModel(model)
+    setCellRenderer(createCellRenderer())
+    HintUpdateSupply.installDataContextHintUpdateSupply(this)
+  }
 }
