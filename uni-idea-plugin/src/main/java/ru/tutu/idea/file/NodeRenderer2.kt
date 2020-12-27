69449efb456f22225ba76931dd3c8d10ab868bb4
avdim <avdim@mail.ru>
2020-12-27 19:53:14 +0300

    simplify NodeRenderer2.kt

--- uni-idea-plugin/src/main/java/ru/tutu/idea/file/NodeRenderer2.kt
+++ uni-idea-plugin/src/main/java/ru/tutu/idea/file/NodeRenderer2.kt
@@ -22,155 +22,145 @@ import javax.swing.Icon
 import javax.swing.JTree
 
 open class NodeRenderer2 : ColoredTreeCellRenderer2() {
-    protected fun fixIconIfNeeded(icon: Icon?, selected: Boolean, hasFocus: Boolean): Icon? {
-        return if (icon != null && !StartupUiUtil.isUnderDarcula() && Registry.`is`(
-                "ide.project.view.change.icon.on.selection",
-                true
-            ) && selected && hasFocus
-        ) {
-            IconLoader.getDarkIcon(icon, true)
-        } else icon
-    }
-
-    override fun customizeCellRenderer(
-        tree: JTree,
-        value: @NlsSafe Any?,
-        selected: Boolean,
-        expanded: Boolean,
-        leaf: Boolean,
-        row: Int,
-        hasFocus: Boolean
+  protected fun fixIconIfNeeded(icon: Icon?, selected: Boolean, hasFocus: Boolean): Icon? {
+    return if (icon != null && !StartupUiUtil.isUnderDarcula() && Registry.`is`(
+        "ide.project.view.change.icon.on.selection",
+        true
+      ) && selected && hasFocus
     ) {
-        val node = TreeUtil.getUserObject(value)
-        if (node is NodeDescriptor<*>) {
-            val descriptor = node
-            // TODO: use this color somewhere
-            val color = descriptor.color
-            icon = fixIconIfNeeded(descriptor.icon, selected, hasFocus)
+      IconLoader.getDarkIcon(icon, true)
+    } else icon
+  }
+
+  override fun customizeCellRenderer(
+    tree: JTree,
+    value: @NlsSafe Any?,
+    selected: Boolean,
+    expanded: Boolean,
+    leaf: Boolean,
+    row: Int,
+    hasFocus: Boolean
+  ) {
+    val node = TreeUtil.getUserObject(value)
+    if (node is NodeDescriptor<*>) {
+      val descriptor = node
+      icon = fixIconIfNeeded(descriptor.icon, selected, hasFocus)
+    }
+    val p0 = getPresentation(node)
+    if (p0 is PresentationData) {
+      val presentation = p0
+      val color = if (node is NodeDescriptor<*>) node.color else null
+      icon = fixIconIfNeeded(presentation.getIcon(false), selected, hasFocus)
+      val coloredText = presentation.coloredText
+      val forcedForeground = presentation.forcedTextForeground
+      if (coloredText.isEmpty()) {
+        var text = presentation.presentableText
+        if (StringUtil.isEmpty(text)) {
+          val valueSting = value.toString()
+          text = valueSting
         }
-        val p0 = getPresentation(node)
-        if (p0 is PresentationData) {
-            val presentation = p0
-            val color = if (node is NodeDescriptor<*>) node.color else null
-            icon = fixIconIfNeeded(presentation.getIcon(false), selected, hasFocus)
-            val coloredText = presentation.coloredText
-            val forcedForeground = presentation.forcedTextForeground
-            if (coloredText.isEmpty()) {
-                var text = presentation.presentableText
-                if (StringUtil.isEmpty(text)) {
-                    val valueSting = value.toString()
-                    text = valueSting
-                }
-                text = tree.convertValueToText(text, selected, expanded, leaf, row, hasFocus)
-                val simpleTextAttributes = getSimpleTextAttributes(
-                    presentation, forcedForeground ?: color, node!!
+        text = tree.convertValueToText(text, selected, expanded, leaf, row, hasFocus)
+        val simpleTextAttributes = getSimpleTextAttributes(
+          presentation, forcedForeground ?: color
+        )
+        append(text, simpleTextAttributes)
+        val location = presentation.locationString
+        if (!StringUtil.isEmpty(location)) {
+          val attributes =
+            SimpleTextAttributes.merge(simpleTextAttributes, SimpleTextAttributes.GRAYED_ATTRIBUTES)
+          append(presentation.locationPrefix + location + presentation.locationSuffix, attributes, false)
+        }
+      } else {
+        var first = true
+        var isMain = true
+        for (each in coloredText) {
+          var simpleTextAttributes = each.attributes
+          if (each.attributes.fgColor == null && forcedForeground != null) {
+            simpleTextAttributes = addColorToSimpleTextAttributes(each.attributes, forcedForeground)
+          }
+          if (first) {
+            val textAttributesKey = presentation.textAttributesKey
+            if (textAttributesKey != null) {
+              val forcedAttributes = scheme.getAttributes(textAttributesKey)
+              if (forcedAttributes != null) {
+                simpleTextAttributes = SimpleTextAttributes.merge(
+                  simpleTextAttributes,
+                  SimpleTextAttributes.fromTextAttributes(forcedAttributes)
                 )
-                append(text, simpleTextAttributes)
-                val location = presentation.locationString
-                if (!StringUtil.isEmpty(location)) {
-                    val attributes =
-                        SimpleTextAttributes.merge(simpleTextAttributes, SimpleTextAttributes.GRAYED_ATTRIBUTES)
-                    append(presentation.locationPrefix + location + presentation.locationSuffix, attributes, false)
-                }
-            } else {
-                var first = true
-                var isMain = true
-                for (each in coloredText) {
-                    var simpleTextAttributes = each.attributes
-                    if (each.attributes.fgColor == null && forcedForeground != null) {
-                        simpleTextAttributes = addColorToSimpleTextAttributes(each.attributes, forcedForeground)
-                    }
-                    if (first) {
-                        val textAttributesKey = presentation.textAttributesKey
-                        if (textAttributesKey != null) {
-                            val forcedAttributes = scheme.getAttributes(textAttributesKey)
-                            if (forcedAttributes != null) {
-                                simpleTextAttributes = SimpleTextAttributes.merge(
-                                    simpleTextAttributes,
-                                    SimpleTextAttributes.fromTextAttributes(forcedAttributes)
-                                )
-                            }
-                        }
-                        first = false
-                    }
-                    // the first grayed text (inactive foreground, regular or small) ends main speed-searchable text
-                    isMain = isMain && !Comparing.equal(
-                        simpleTextAttributes.fgColor,
-                        SimpleTextAttributes.GRAYED_ATTRIBUTES.fgColor
-                    )
-                    append(each.text, simpleTextAttributes, isMain)
-                }
-                val location = presentation.locationString
-                if (!StringUtil.isEmpty(location)) {
-                    append(
-                        presentation.locationPrefix + location + presentation.locationSuffix,
-                        SimpleTextAttributes.GRAYED_ATTRIBUTES,
-                        false
-                    )
-                }
+              }
             }
-            toolTipText = presentation.tooltip
-        } else if (value != null) {
-            var text: @NlsSafe String? = value.toString()
-            if (node is NodeDescriptor<*>) {
-                text = node.toString()
-            }
-            text = tree.convertValueToText(text, selected, expanded, leaf, row, hasFocus)
-            if (text == null) {
-                text = ""
-            }
-            append(text)
-            toolTipText = null
+            first = false
+          }
+          // the first grayed text (inactive foreground, regular or small) ends main speed-searchable text
+          isMain = isMain && !Comparing.equal(
+            simpleTextAttributes.fgColor,
+            SimpleTextAttributes.GRAYED_ATTRIBUTES.fgColor
+          )
+          append(each.text, simpleTextAttributes, isMain)
         }
+        val location = presentation.locationString
+        if (!StringUtil.isEmpty(location)) {
+          append(
+            presentation.locationPrefix + location + presentation.locationSuffix,
+            SimpleTextAttributes.GRAYED_ATTRIBUTES,
+            false
+          )
+        }
+      }
+      toolTipText = presentation.tooltip
+    } else if (value != null) {
+      var text: @NlsSafe String? = value.toString()
+      if (node is NodeDescriptor<*>) {
+        text = node.toString()
+      }
+      text = tree.convertValueToText(text, selected, expanded, leaf, row, hasFocus)
+      if (text == null) {
+        text = ""
+      }
+      append(text)
+      toolTipText = null
     }
+  }
 
-    protected fun getPresentation(node: Any?): ItemPresentation? {
-        return if (node is PresentableNodeDescriptor<*>) node.presentation else if (node is NavigationItem) node.presentation else null
-    }
+  protected fun getPresentation(node: Any?): ItemPresentation? {
+    return if (node is PresentableNodeDescriptor<*>) node.presentation else if (node is NavigationItem) node.presentation else null
+  }
 
-    protected fun getSimpleTextAttributes(
-        presentation: PresentationData,
-        color: Color?,
-        node: Any
+  protected fun getSimpleTextAttributes(presentation: PresentationData, color: Color?): SimpleTextAttributes {
+    val simpleTextAttributes = getSimpleTextAttributes(presentation, scheme)
+    return addColorToSimpleTextAttributes(simpleTextAttributes, color)
+  }
+
+  companion object {
+    private val scheme: EditorColorsScheme
+      private get() = EditorColorsManager.getInstance().schemeForCurrentUITheme
+
+    private fun addColorToSimpleTextAttributes(
+      simpleTextAttributes: SimpleTextAttributes,
+      color: Color?
     ): SimpleTextAttributes {
-        val simpleTextAttributes = getSimpleTextAttributes(presentation, scheme)
-        return addColorToSimpleTextAttributes(simpleTextAttributes, color)
+      var simpleTextAttributes = simpleTextAttributes
+      if (color != null) {
+        val textAttributes = simpleTextAttributes.toTextAttributes()
+        textAttributes.foregroundColor = color
+        simpleTextAttributes = SimpleTextAttributes.fromTextAttributes(textAttributes)
+      }
+      return simpleTextAttributes
     }
 
-    companion object {
-        private val scheme: EditorColorsScheme
-            private get() = EditorColorsManager.getInstance().schemeForCurrentUITheme
-
-        private fun addColorToSimpleTextAttributes(
-            simpleTextAttributes: SimpleTextAttributes,
-            color: Color?
-        ): SimpleTextAttributes {
-            var simpleTextAttributes = simpleTextAttributes
-            if (color != null) {
-                val textAttributes = simpleTextAttributes.toTextAttributes()
-                textAttributes.foregroundColor = color
-                simpleTextAttributes = SimpleTextAttributes.fromTextAttributes(textAttributes)
-            }
-            return simpleTextAttributes
-        }
-
-        fun getSimpleTextAttributes(presentation: ItemPresentation?): SimpleTextAttributes {
-            return getSimpleTextAttributes(presentation, scheme)
-        }
-
-        private fun getSimpleTextAttributes(
-            presentation: ItemPresentation?,
-            colorsScheme: EditorColorsScheme
-        ): SimpleTextAttributes {
-            if (presentation is ColoredItemPresentation) {
-                val textAttributesKey = presentation.textAttributesKey
-                    ?: return SimpleTextAttributes.REGULAR_ATTRIBUTES
-                val textAttributes = colorsScheme.getAttributes(textAttributesKey)
-                return if (textAttributes == null) SimpleTextAttributes.REGULAR_ATTRIBUTES else SimpleTextAttributes.fromTextAttributes(
-                    textAttributes
-                )
-            }
-            return SimpleTextAttributes.REGULAR_ATTRIBUTES
-        }
+    private fun getSimpleTextAttributes(
+      presentation: ItemPresentation?,
+      colorsScheme: EditorColorsScheme
+    ): SimpleTextAttributes {
+      if (presentation is ColoredItemPresentation) {
+        val textAttributesKey = presentation.textAttributesKey
+          ?: return SimpleTextAttributes.REGULAR_ATTRIBUTES
+        val textAttributes = colorsScheme.getAttributes(textAttributesKey)
+        return if (textAttributes == null) SimpleTextAttributes.REGULAR_ATTRIBUTES else SimpleTextAttributes.fromTextAttributes(
+          textAttributes
+        )
+      }
+      return SimpleTextAttributes.REGULAR_ATTRIBUTES
     }
+  }
 }
