6ab388f5694cbfddd0710003a02fb10cdc60a0af
avdim <avdim@mail.ru>
2021-01-26 11:11:24 +0300

    gradle java

--- uni-idea-plugin/build.gradle.kts
+++ uni-idea-plugin/build.gradle.kts
@@ -31,8 +31,16 @@ buildConfigKotlin {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  if (true) {
+    toolchain {
+      languageVersion.set(JavaLanguageVersion.of(11))
+      vendor.set(JvmVendorSpec.BELLSOFT)
+      implementation.set(JvmImplementation.VENDOR_SPECIFIC)
+    }
+  } else {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
 }
 
 repositories {
