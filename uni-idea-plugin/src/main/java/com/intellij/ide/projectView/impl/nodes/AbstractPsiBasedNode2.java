4dde87a2b8b40b0801f81196782df9ab67e4241e
avdim <avdim@mail.ru>
2021-01-30 23:00:27 +0300

    AbstractPsiBasedNode2

--- uni-idea-plugin/src/main/java/com/intellij/ide/projectView/impl/nodes/AbstractPsiBasedNode2.java
+++ uni-idea-plugin/src/main/java/com/intellij/ide/projectView/impl/nodes/AbstractPsiBasedNode2.java
@@ -3,23 +3,40 @@
 package com.intellij.ide.projectView.impl.nodes;
 
 import com.intellij.codeInsight.navigation.NavigationUtil;
+import com.intellij.ide.*;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ProjectViewSettings;
 import com.intellij.ide.projectView.ViewSettings;
 
 import com.intellij.ide.util.treeView.ValidateableNode;
 import com.intellij.navigation.NavigationItem;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ApplicationNamesInfo;
+import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.*;
 import com.intellij.openapi.editor.colors.CodeInsightColors;
+import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.fileEditor.impl.EditorHistoryManager;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.fileTypes.INativeFileType;
+import com.intellij.openapi.fileTypes.UnknownFileType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Iconable;
+import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vfs.NonPhysicalFileSystem;
 import com.intellij.openapi.vfs.VFileProperty;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.pom.StatePreservingNavigatable;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
@@ -30,13 +47,16 @@ import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.ui.LayeredIcon;
 import com.intellij.util.AstLoadingFilter;
 import com.intellij.util.PlatformIcons;
+import com.unicorn.Uni;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Class for node descriptors based on PsiElements. Subclasses should define
@@ -216,7 +236,12 @@ public abstract class AbstractPsiBasedNode2<Value> extends ProjectViewNode2B<Val
   public void navigate(boolean requestFocus, boolean preserveState) {
     if (canNavigate()) {
       if (requestFocus || preserveState) {
-        NavigationUtil.openFileWithPsiElement(extractPsiFromValue(), requestFocus, requestFocus);
+        if (true) {
+          openFileWithPsiElement(getVirtualFile(), extractPsiFromValue(), requestFocus, requestFocus);
+        } else {
+          //old stable
+          NavigationUtil.openFileWithPsiElement(extractPsiFromValue(), requestFocus, requestFocus);
+        }
       } else {
         getNavigationItem().navigate(false);
       }
@@ -249,4 +274,192 @@ public abstract class AbstractPsiBasedNode2<Value> extends ProjectViewNode2B<Val
 
     return getValue() != null;
   }
+
+  @NotNull
+  public static TextRange getRangeToUnfoldOnNavigation(@NotNull Editor editor) {
+    final int offset = editor.getCaretModel().getOffset();
+    int line = editor.getDocument().getLineNumber(offset);
+    int start = editor.getDocument().getLineStartOffset(line);
+    int end = editor.getDocument().getLineEndOffset(line);
+    return new TextRange(start, end);
+  }
+
+  protected static void unfoldCurrentLine(@NotNull final Editor editor) {
+    final FoldRegion[] allRegions = editor.getFoldingModel().getAllFoldRegions();
+    final TextRange range = getRangeToUnfoldOnNavigation(editor);
+    editor.getFoldingModel().runBatchFoldingOperation(() -> {
+      for (FoldRegion region : allRegions) {
+        if (!region.isExpanded() && range.intersects(TextRange.create(region))) {
+          region.setExpanded(true);
+        }
+      }
+    });
+  }
+
+  private static boolean openFileWithPsiElement(VirtualFile file, PsiElement element, boolean searchForOpen, boolean requestFocus) {
+    boolean openAsNative = false;
+    if (element instanceof PsiFile) {
+      VirtualFile virtualFile = ((PsiFile) element).getVirtualFile();
+      if (virtualFile != null) {
+        FileType type = virtualFile.getFileType();
+        openAsNative = type instanceof INativeFileType || type instanceof UnknownFileType;
+      }
+    }
+
+    if (searchForOpen) {
+      element.putUserData(FileEditorManager.USE_CURRENT_WINDOW, null);
+    } else {
+      element.putUserData(FileEditorManager.USE_CURRENT_WINDOW, true);
+    }
+
+    Ref<Boolean> resultRef = new Ref<>();
+    boolean openAsNativeFinal = openAsNative;
+    // all navigation inside should be treated as a single operation, so that 'Back' action undoes it in one go
+    @NotNull Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+    for (Project proj : openProjects) {
+      element.getProject();
+      CommandProcessor.getInstance().executeCommand(proj, () -> {
+        if (openAsNativeFinal || !activatePsiElementIfOpen(element, searchForOpen, requestFocus)) {
+          final NavigationItem navigationItem = (NavigationItem) element;
+          if (!navigationItem.canNavigate()) {
+            resultRef.set(Boolean.FALSE);
+          } else {
+            navigate2(proj, file, requestFocus);
+//            navigationItem.navigate(requestFocus);
+            resultRef.set(Boolean.TRUE);
+          }
+        }
+      }, "", null);
+    }
+
+    if (!resultRef.isNull()) return resultRef.get();
+
+    element.putUserData(FileEditorManager.USE_CURRENT_WINDOW, null);
+    return false;
+  }
+
+  public static void navigate2(Project project, @NotNull VirtualFile virtualFile, boolean requestFocus) {
+    if (!virtualFile.isValid()) {
+      throw new IllegalStateException("target not valid");
+    }
+
+    if (!virtualFile.isDirectory()) {
+      if (navigateInEditorOrNativeApp(project, virtualFile, requestFocus)) return;
+    }
+
+    if (navigateInProjectView(project, virtualFile, requestFocus)) return;
+
+    String message = IdeBundle.message("error.files.of.this.type.cannot.be.opened", ApplicationNamesInfo.getInstance().getProductName());
+    Messages.showErrorDialog(project, message, IdeBundle.message("title.cannot.open.file"));
+  }
+
+  private static boolean navigateInEditorOrNativeApp(Project project, VirtualFile file, boolean requestFocus) {
+    FileType type = FileTypeManager.getInstance().getKnownFileTypeOrAssociate(file, project);
+    if (type == null || !file.isValid()) return false;
+
+    if (type instanceof INativeFileType) {
+      return ((INativeFileType) type).openFileInAssociatedApplication(project, file);
+    }
+
+    return navigateInEditor(project, file, requestFocus);
+  }
+
+  public static boolean navigateInEditor(Project project, VirtualFile file, boolean requestFocus) {
+    return navigateInRequestedEditor(project, file) || navigateInAnyFileEditor(project, file, requestFocus);
+  }
+
+  protected static boolean navigateInAnyFileEditor(Project project, VirtualFile file, boolean focusEditor) {
+    FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+
+    FileEditor[] editors = fileEditorManager.openFile(file, focusEditor);
+    for (FileEditor editor : editors) {
+      if (editor instanceof TextEditor) {
+        Editor e = ((TextEditor) editor).getEditor();
+        fileEditorManager.runWhenLoaded(e, () -> {
+          unfoldCurrentLine(e);
+          if (focusEditor) {
+            IdeFocusManager.getInstance(project).requestFocus(e.getContentComponent(), true);
+            Window ancestor = SwingUtilities.getWindowAncestor(e.getContentComponent());
+            if (ancestor != null) {
+              ancestor.toFront();
+            }
+          }
+        });
+      }
+    }
+    return editors.length > 0;
+  }
+
+  private static boolean navigateInRequestedEditor(Project project, @NotNull VirtualFile file) {
+    @SuppressWarnings("deprecation") DataContext ctx = DataManager.getInstance().getDataContext();
+    Editor e = OpenFileDescriptor.NAVIGATE_IN_EDITOR.getData(ctx);
+    if (e == null) return false;
+    if (e.isDisposed()) {
+      Logger.getInstance(OpenFileDescriptor.class).error("Disposed editor returned for NAVIGATE_IN_EDITOR from " + ctx);
+      return false;
+    }
+    if (!Comparing.equal(FileDocumentManager.getInstance().getFile(e.getDocument()), file)) return false;
+
+    navigateInEditor(project, file, e);
+    return true;
+  }
+
+  protected static void navigateInEditor(Project project, @NotNull VirtualFile file, @NotNull Editor e) {
+    CaretModel caretModel = e.getCaretModel();
+    boolean caretMoved = false;
+    if (caretMoved) {
+      e.getSelectionModel().removeSelection();
+      FileEditorManager.getInstance(project).runWhenLoaded(e, () -> {
+        e.getScrollingModel().scrollToCaret(ScrollType.CENTER);
+        unfoldCurrentLine(e);
+      });
+    }
+  }
+
+  private static boolean navigateInProjectView(@NotNull Project project, @NotNull VirtualFile file, boolean requestFocus) {
+    SelectInContext context = new FileSelectInContext(project, file, null);
+    for (SelectInTarget target : SelectInManager.getInstance(project).getTargetList()) {
+      if (context.selectIn(target, requestFocus)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private static boolean activatePsiElementIfOpen(@NotNull PsiElement elt, boolean searchForOpen, boolean requestFocus) {
+    if (!elt.isValid()) return false;
+    elt = elt.getNavigationElement();
+    final PsiFile file = elt.getContainingFile();
+    if (file == null || !file.isValid()) return false;
+
+    VirtualFile vFile = file.getVirtualFile();
+    if (vFile == null) return false;
+
+    if (!EditorHistoryManager.getInstance(elt.getProject()).hasBeenOpen(vFile)) return false;
+
+    final FileEditorManager fem = FileEditorManager.getInstance(elt.getProject());
+    if (!fem.isFileOpen(vFile)) {
+      fem.openFile(vFile, requestFocus, searchForOpen);
+    }
+
+    final TextRange range = elt.getTextRange();
+    if (range == null) return false;
+
+    final FileEditor[] editors = fem.getEditors(vFile);
+    for (FileEditor editor : editors) {
+      if (editor instanceof TextEditor) {
+        final Editor text = ((TextEditor) editor).getEditor();
+        final int offset = text.getCaretModel().getOffset();
+
+        if (range.containsOffset(offset)) {
+          // select the file
+          fem.openFile(vFile, requestFocus, searchForOpen);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
 }
