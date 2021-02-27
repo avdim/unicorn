f512908042b6205262b963fc3b2a0b3737d7c312
avdim <avdim@mail.ru>
2021-02-27 20:42:31 +0300

    1500 mb

--- uni-idea-plugin/build.gradle.kts
+++ uni-idea-plugin/build.gradle.kts
@@ -129,7 +129,7 @@ tasks.withType<org.jetbrains.intellij.tasks.RunIdeTask> {
 //  jbrVersion("jbrsdk-11_0_7b989.1")
   systemProperties["ide.browser.jcef.enabled"] = true
 //  systemProperties["pdf.viewer.debug"] = true
-  jvmArgs("-Xmx2500m", "-Xms128m")
+  jvmArgs("-Xmx1500m", "-Xms128m")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
