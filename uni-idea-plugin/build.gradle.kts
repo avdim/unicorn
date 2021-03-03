bb00d7470e7a833394abcb4e552a16bf3cba4ce5
Dmitriy <avdim@mail.ru>
2021-03-03 22:49:42 +0300

    Update build.gradle.kts

--- uni-idea-plugin/build.gradle.kts
+++ uni-idea-plugin/build.gradle.kts
@@ -129,7 +129,7 @@ tasks.withType<org.jetbrains.intellij.tasks.RunIdeTask> {
 //  jbrVersion("jbrsdk-11_0_7b989.1")
   systemProperties["ide.browser.jcef.enabled"] = true
 //  systemProperties["pdf.viewer.debug"] = true
-  jvmArgs("-Xmx1900m", "-Xms128m")
+  jvmArgs("-Xmx2400m", "-Xms128m")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
