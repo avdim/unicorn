ac4fc9e381e385173242467d7416adb14aedd88c
avdim <avdim@mail.ru>
2021-04-16 12:36:54 +0300

    total duration

--- app-github-workflow/src/main/kotlin/org/sample/github/AppGithubWorkflowStarter.kt
+++ app-github-workflow/src/main/kotlin/org/sample/github/AppGithubWorkflowStarter.kt
@@ -3,6 +3,9 @@ package org.sample.github
 import com.sample.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.*
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
@@ -16,7 +19,32 @@ import kotlin.io.path.absolutePathString
 import kotlin.io.path.writeText
 
 const val TUTU_ORGANIZATION = "tutu-ru-mobile"
-const val IOS_REPO = "ios-core"
+val SINCE_TIME = LocalDateTime(2021, Month.MARCH, 29, 0, 0).toInstant(TimeZone.UTC)
+
+enum class RepoType {
+  IOS,
+  ANDROID
+}
+
+data class RepoWithType(
+  val repo: String,
+  val type: RepoType
+)
+
+class RepoWithJobs(
+  val jobs: List<WorkflowRunJob>,
+  val repoWithType: RepoWithType
+)
+
+val REPOS = listOf(
+  RepoWithType("ios-core", RepoType.IOS),
+  RepoWithType("feed-ios", RepoType.IOS),
+  RepoWithType("ios-solution", RepoType.IOS),
+  RepoWithType("ios-tutu-id", RepoType.IOS),
+  RepoWithType("ios-foundation", RepoType.IOS),
+  RepoWithType("ios-designkit", RepoType.IOS),
+  RepoWithType("iosApp_Etrain", RepoType.IOS),
+)
 
 @OptIn(ExperimentalPathApi::class)
 suspend fun main() {
@@ -24,24 +52,36 @@ suspend fun main() {
   println("token: ${BuildConfig.SECRET_GITHUB_TOKEN}")
   val client = HttpClient(Apache)
 
-  val githubMail = client.getGithubMail(Token(BuildConfig.SECRET_GITHUB_TOKEN))
-  println("githubMail: $githubMail")
+  if (false) {
+    val githubMail = client.getGithubMail(Token(BuildConfig.SECRET_GITHUB_TOKEN))
+    println("githubMail: $githubMail")
+  }
 
-  val jobs = client.getGithubWorkflowRunsPagesUntil(Token(BuildConfig.SECRET_GITHUB_TOKEN), TUTU_ORGANIZATION, IOS_REPO) {
-//    it.createdTime > LocalDateTime(2021, Month.MARCH, 29, 0, 0).toInstant(TimeZone.UTC)
-    it.createdTime > LocalDateTime(2021, Month.MARCH, 29, 0, 0).toInstant(TimeZone.UTC)
-  }.flatMapMerge { run ->
-    flow {
-      emit(
-        client.getGithubWorkflowRunJobs(Token(BuildConfig.SECRET_GITHUB_TOKEN), TUTU_ORGANIZATION, IOS_REPO, run.id)
-      )
-    }
-  }.mapNotNull {
-    if (it is Response.Success) it else null
-  }.completeToList()
-    .flatMap {
-      it.data.jobs
-    }
+  val allRepos = coroutineScope {
+    REPOS.map { repoWithType ->
+      async {
+        val jobs = client.getGithubWorkflowRunsPagesUntil(Token(BuildConfig.SECRET_GITHUB_TOKEN), TUTU_ORGANIZATION, repoWithType.repo) {
+          it.createdTime > SINCE_TIME
+        }.flatMapMerge { run ->
+          flow {
+            emit(
+              client.getGithubWorkflowRunJobs(Token(BuildConfig.SECRET_GITHUB_TOKEN), TUTU_ORGANIZATION, repoWithType.repo, run.id)
+            )
+          }
+        }.mapNotNull {
+          if (it is Response.Success) it else null
+        }.completeToList()
+          .flatMap {
+            it.data.jobs
+          }
+        RepoWithJobs(jobs, repoWithType)
+      }
+    }.awaitAll()
+  }
+
+  val totalDuration = allRepos.fold(WrapDuration(0)) { acc, value ->
+    acc + value.jobs.calcDuration()
+  }
 
   val htmlFile = Files.createTempFile("github_time", ".html")
   println("file://${htmlFile.absolutePathString()}")
@@ -63,25 +103,40 @@ suspend fun main() {
             }
           }
           body {
-            table {
-              jobs.forEach { job ->
-                tr {
-                  td {
-                    val sec = job.calcDuration().seconds
-                    +"$sec"
-                  }
-                  td {
-                    val start = job.startedAt
-                    +"$start"
-                  }
-                  td {
-                    a(job.htmlUrl) {
-                      target = ATarget.blank
-                      +"logs"
+            h3 {
+              +"total duration ${totalDuration} since $SINCE_TIME"
+            }
+            allRepos.forEach { repo ->
+              br {}
+              br {}
+              h2 {
+                +"time: ${repo.jobs.calcDuration()} in repo ${repo.repoWithType.repo}"
+              }
+              br {}
+              table {
+                repo.jobs.forEach { job ->
+                  val sec = job.calcDuration().seconds
+                  tr {
+                    td {
+                      +"$sec"
+                    }
+                    td {
+                      +"${sec / 60 + 1} m"
+                    }
+                    td {
+                      val start = job.startedAt
+                      +"$start"
+                    }
+                    td {
+                      a(job.htmlUrl) {
+                        target = ATarget.blank
+                        +"logs"
+                      }
                     }
                   }
                 }
               }
+
             }
           }
         }
@@ -89,6 +144,12 @@ suspend fun main() {
     )
 
 }
+
+fun Collection<WorkflowRunJob>.calcDuration() =
+  map { it.calcDuration() }
+    .fold(WrapDuration(0)) { acc, value ->
+      acc + value
+    }
 
 suspend inline fun <T> Flow<T>.completeToList(): List<T> =
   fold(emptyList<T>()) { acc, value ->
--- app-github-workflow/src/main/kotlin/org/sample/github/WrapDuration.kt
+++ app-github-workflow/src/main/kotlin/org/sample/github/WrapDuration.kt
@@ -1,6 +1,10 @@
 package org.sample.github
 
-inline class WrapDuration(val seconds: Long)
+inline class WrapDuration(val seconds: Long) {
+  override fun toString(): String {
+    return "${seconds / 60 } m + ${seconds % 60} s"
+  }
+}
 
 fun Collection<WrapDuration>.sum(): WrapDuration {
   val sumSeconds = map {
