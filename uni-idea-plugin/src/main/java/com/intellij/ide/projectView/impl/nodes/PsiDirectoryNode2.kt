a832bc0659d444aa1b0f2b14b617635df08e1b5e
avdim <avdim@mail.ru>
2021-04-24 18:09:21 +0300

    rm chain

--- uni-idea-plugin/src/main/java/com/intellij/ide/projectView/impl/nodes/PsiDirectoryNode2.kt
+++ uni-idea-plugin/src/main/java/com/intellij/ide/projectView/impl/nodes/PsiDirectoryNode2.kt
@@ -12,38 +12,13 @@ import com.unicorn.Uni
 import com.unicorn.Uni.BOLD_DIRS
 
 abstract class PsiDirectoryNode2(val virtualDir:VirtualFile) : BasePsiNode2(virtualDir), NavigatableWithText {
-  // the chain from a parent directory to this one usually contains only one virtual file
-  private val chain: MutableSet<VirtualFile> = SmartHashSet()
   override fun updateImpl(data: PresentationData) {
-    val parentValue = parentValue
-    synchronized(chain) {
-      if (chain.isEmpty()) {
-        val ancestor = getVirtualFile(parentValue)
-        if (ancestor != null) {
-          var file: VirtualFile? = virtualDir
-          while (file != null && VfsUtilCore.isAncestor(ancestor, file, true)) {
-            chain.add(file)
-            file = file.parent
-          }
-        }
-        if (chain.isEmpty()) chain.add(virtualDir)
-      }
-    }
     if (BOLD_DIRS) {
       data.addText(virtualDir.name + " ", SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES)
     }
   }
-
-  val isFQNameShown: Boolean
-    get() = false
-
-  override fun isValid(): Boolean {
-    return true
-    //    if (!super.isValid()) return false;
-//    return ProjectViewDirectoryHelper.getInstance(getProject())
-//      .isValidDirectory(getValue(), getParentValue(), getSettings(), getFilter());
-  }
-
+  val isFQNameShown: Boolean get() = false
+  override fun isValid(): Boolean = true
   override fun canNavigate(): Boolean = false
   override fun canNavigateToSource(): Boolean = false
   override fun navigate(requestFocus: Boolean, preserveState: Boolean) {}
@@ -58,15 +33,4 @@ abstract class PsiDirectoryNode2(val virtualDir:VirtualFile) : BasePsiNode2(virt
 
   override val isAlwaysShowPlus get(): Boolean = getVirtualFile().children.isNotEmpty()
 
-  companion object {
-    /**
-     * @return a virtual file that identifies the given element
-     */
-    private fun getVirtualFile(element: Any?): VirtualFile? {
-      if (element is PsiDirectory) {
-        return element.virtualFile
-      }
-      return if (element is VirtualFile) element else null
-    }
-  }
 }
