e26ca55e25b0234991e177a9cee63abd268df5ae
avdim <avdim@mail.ru>
2021-10-11 10:56:42 +0300

    val INTELLIJ_GRADLE = "1.2.0"

--- buildSrc/src/main/kotlin/const.kt
+++ buildSrc/src/main/kotlin/const.kt
@@ -66,12 +66,8 @@ val LAST_COMMUNITY = IdeaVersion.Download(LAST_IDEA_STR, "IC")
 val LAST_ULTIMATE = IdeaVersion.Download(LAST_IDEA_STR, "IU")
 
 // https://github.com/JetBrains/gradle-intellij-plugin
-//val INTELLIJ_GRADLE = "0.7.2"
-//val INTELLIJ_GRADLE = "0.7.3"
-//val INTELLIJ_GRADLE = "1.0"
-//val INTELLIJ_GRADLE = "1.1.2"
-//val INTELLIJ_GRADLE = "1.1.4"
-val INTELLIJ_GRADLE = "1.1.6"
+//val INTELLIJ_GRADLE = "1.1.6"
+val INTELLIJ_GRADLE = "1.2.0"
 // https://maven.pkg.jetbrains.space/public/p/compose/dev/org/jetbrains/compose/org.jetbrains.compose.gradle.plugin/
 //val DESKTOP_COMPOSE = "0.3.0-build152"
 //val DESKTOP_COMPOSE = "0.3.2"
