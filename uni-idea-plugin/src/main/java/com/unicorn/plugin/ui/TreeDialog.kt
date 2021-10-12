6cb6ef42c8eb9e7cb097552f891d91e17e092a8f
avdim <avdim@mail.ru>
2021-10-12 18:37:55 +0300

    MutableListTreeNode

--- uni-idea-plugin/src/main/java/com/unicorn/plugin/ui/TreeDialog.kt
+++ uni-idea-plugin/src/main/java/com/unicorn/plugin/ui/TreeDialog.kt
@@ -5,7 +5,6 @@ import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.TreeSpeedSearch
 import com.intellij.ui.components.JBList
 import com.intellij.ui.treeStructure.Tree
-import com.intellij.util.containers.Convertor
 import com.unicorn.Uni
 import com.unicorn.plugin.TreeAction
 import com.unicorn.plugin.TreeState
@@ -13,13 +12,36 @@ import com.unicorn.plugin.ui.render.stateFlowView
 import ru.avdim.mvi.createStore
 import java.util.*
 import javax.swing.JTree
-import javax.swing.tree.TreeNode
-import javax.swing.tree.TreePath
+import javax.swing.tree.*
 
 fun showTreeDialog() {
   val store = createStore(TreeState(0)) { s, a: TreeAction -> s.copy(s.i + 1) }
   showPanelDialog(Uni) {
     Uni.scope.stateFlowView(this, store.stateFlow) {
+
+      val mutableListTreeNode = MutableListTreeNode(
+        "root",
+        Leaf("aaa"),
+        MutableListTreeNode(
+          "2",
+          Leaf("2aaa"),
+          Leaf("2bbb"),
+        ),
+        Leaf("bbb"),
+      )
+      val defaultTreeModel = DefaultTreeModel(mutableListTreeNode)
+
+      row {
+        button("add new") {
+          defaultTreeModel.insertNodeInto(
+            Leaf("new"),
+            mutableListTreeNode,
+            0
+          )
+          mutableListTreeNode.remove(0)
+        }
+      }
+
       row {
         val list = JBList<String>()
 //        list.setDataProvider(DataProvider { listOf("a", "b") })
@@ -30,18 +52,7 @@ fun showTreeDialog() {
       }
       row {
         label("Tree view")
-        val tree = Tree(
-          ListTreeNode(
-            "root",
-            Leaf("aaa"),
-            ListTreeNode2(
-              "2",
-              Leaf("2aaa"),
-              Leaf("2bbb"),
-            ),
-            Leaf("bbb"),
-          )
-        )
+        val tree = Tree(defaultTreeModel)
         tree.setCellRenderer(object : ColoredTreeCellRenderer() {
           override fun customizeCellRenderer(
             tree: JTree,
@@ -59,50 +70,25 @@ fun showTreeDialog() {
         })
         TreeSpeedSearch(tree) { it?.lastPathComponent?.toString() ?: "empty" }
         tree()
+        tree.model
       }
     }
   }
 }
 
-fun Leaf(content: String): TreeNode {
-  return object : TreeNode {
-    override fun getChildAt(childIndex: Int): TreeNode = TODO()
-    override fun getChildCount(): Int = 0
-    override fun getParent(): TreeNode = this
-    override fun getIndex(node: TreeNode?): Int = -1
-    override fun getAllowsChildren(): Boolean = false
-    override fun isLeaf(): Boolean = true
-    override fun children(): Enumeration<out TreeNode> =
-      Collections.enumeration(emptyList())
+fun Leaf(content: String): MutableTreeNode {
+  return DefaultMutableTreeNode(content, false)
+}
 
-    override fun toString(): String = content
+fun MutableListTreeNode(text: String, vararg items: MutableTreeNode): MutableTreeNode {
+  val node = DefaultMutableTreeNode(text, true)
+  items.forEach {
+    node.add(it)
   }
+  return node
 }
 
 fun ListTreeNode(text: String, vararg items: TreeNode): TreeNode {
-  return object : TreeNode {
-    val listParent = this
-    val childs: List<TreeNode> = items.map {
-      object : TreeNode by it {
-        override fun getParent(): TreeNode = listParent
-        override fun toString(): String = it.toString()
-      }
-    }
-
-    override fun getChildAt(childIndex: Int): TreeNode = childs[childIndex]
-    override fun getChildCount(): Int = childs.size
-    override fun getParent(): TreeNode = this
-    override fun getIndex(node: TreeNode?): Int = node?.let { childs.indexOf(it) } ?: -1
-    override fun getAllowsChildren(): Boolean = true
-    override fun isLeaf(): Boolean = false
-    override fun children(): Enumeration<out TreeNode> =
-      Collections.enumeration(childs)
-
-    override fun toString(): String = text
-  }
-}
-
-fun ListTreeNode2(text: String, vararg items: TreeNode): TreeNode {
   return object : TreeNode {
     val listParent = this
     val childs: List<TreeNode> = items.map {
