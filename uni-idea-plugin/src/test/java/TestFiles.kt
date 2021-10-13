6c64a4a50f77bc6390b566f36395af357a9ead92
avdim <avdim@mail.ru>
2021-10-13 13:43:24 +0300

    TestFiles

---
+++ uni-idea-plugin/src/test/java/TestFiles.kt
@@ -0,0 +1,46 @@
+import org.junit.Test
+import java.nio.file.*
+import java.util.concurrent.TimeUnit
+import java.util.stream.Stream
+
+class TestFiles {
+//  @Test
+  fun testTmpDir() {
+    val path = Paths.get("/tmp")
+    Files.createDirectories(path)
+    val list: Stream<Path> = Files.list(path)
+    list.use {
+      it.forEach {
+        println(it)
+      }
+    }
+
+    val ws = path.fileSystem.newWatchService()
+    path.register(
+      ws,
+      StandardWatchEventKinds.OVERFLOW,
+      StandardWatchEventKinds.ENTRY_CREATE,
+      StandardWatchEventKinds.ENTRY_DELETE,
+      StandardWatchEventKinds.ENTRY_MODIFY
+    )
+
+    while (true) {
+      val key: WatchKey = try {
+        ws.take()
+      } catch (t: Throwable) {
+        println("catch")
+        continue
+      }
+      try {
+        val events = key.pollEvents()
+        events.forEach {
+          val kind = it.kind()
+          val context = it.context()
+          println("kind: $kind, context: $context")
+        }
+      } finally {
+        key.reset()
+      }
+    }
+  }
+}
