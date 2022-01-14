595b14fb87fa8ddc8515eef1e9c4db09e7ce7e30
Dmitry <avdim@mail.ru>
2022-01-14 09:43:38 +0300

    xmx 3500

--- uni-idea-plugin/build.gradle.kts
+++ uni-idea-plugin/build.gradle.kts
@@ -163,7 +163,7 @@ tasks.withType<org.jetbrains.intellij.tasks.RunIdeTask> {
 //  jbrVersion("jbr_dcevm-11_0_10-linux-x64-b1341.35")
   systemProperties["ide.browser.jcef.enabled"] = true
 //  systemProperties["pdf.viewer.debug"] = true
-  jvmArgs("-Xmx2548m", "-Xms128m")
+  jvmArgs("-Xmx3500m", "-Xms128m")
   autoReloadPlugins.set(true)
 }
 
